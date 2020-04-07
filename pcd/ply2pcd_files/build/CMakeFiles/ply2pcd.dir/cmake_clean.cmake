e2b130bdb1c1982f6ca0b9c15ec02d05a3eca6f9
panpan2 <p.panayiotou2@gmail.com>
2020-04-07 16:20:56 +0200

    Add ply2pcd conversion tool

---
+++ pcd/ply2pcd_files/build/CMakeFiles/ply2pcd.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ply2pcd.dir/ply2pcd.cpp.o"
+  "ply2pcd.pdb"
+  "ply2pcd"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ply2pcd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
